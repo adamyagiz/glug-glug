65b4f0c56ff682740c5a1e20235e60e613d68d2b
Adam Yagiz <akyagiz@gmail.com>
2018-01-25 13:34:34 -0800

    1. Code clean-up. 2. Moved easter egg out of beta.

--- glug-glug.js
+++ glug-glug.js
@@ -1,8 +1,6 @@
-'use strict';
-
-module.exports = function(wrld) {
-  let world = wrld ? wrld : 'world';
-  if (world == 'wut') {
+module.exports = (wrld) => {
+  const world = wrld || 'world';
+  if (world === 'wut') {
     console.log(`${world} in the world!?`);
   } else {
     console.log(`Hello, ${world}!!`);
