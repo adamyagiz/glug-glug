a82017908303d361aef402a159fd0d6ae4f8f031
Adam Yagiz <akyagiz@gmail.com>
2018-01-26 15:02:35 -0800

    Node modules don't support ES6 features?

--- glug-glug.js
+++ glug-glug.js
@@ -1,8 +1,10 @@
-module.exports = (wrld) => {
-  const world = wrld || 'world';
+'use strict';
+
+module.exports = function(wrld) {
+  var world = wrld || 'world';
   if (world === 'wut') {
-    console.log(`${world} in the world!?`);
+    console.log(world + ' in the world!?');
   } else {
-    console.log(`Hello, ${world}!!`);
+    console.log('Hello, ' + world + '!!');
   }
 };
